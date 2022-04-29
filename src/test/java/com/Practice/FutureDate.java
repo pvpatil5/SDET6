7f7ea39863bca3f786d2f94f64948bbc69412048
pvpatil5 <pvpatil123@yahoo.com>
2022-04-29 10:58:55 +0530

    FutureDate

--- src/test/java/com/Practice/FutureDate.java
+++ src/test/java/com/Practice/FutureDate.java
@@ -5,34 +5,33 @@ import java.util.Date;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class BrowserLAunch {
+public class FutureDate {
 
 	public static void main(String[] args) throws InterruptedException {
-
-		
 		Date date = new  Date();
-		
-		String currentdate=date.toString();
-		
-		//Thu Apr 28 18:09:03 IST 2022
-		// 0   1   2    3      4    5 
-		// Thu Apr 28 2022
-		
-		String[] arr = currentdate.split(" ");
-		
-		String day= arr[0];
-		String month= arr[1];
-		String dt= arr[2];
-		String year = arr[5];
-		
-		String xpath= day+" "+month+" "+dt+" "+year;
-		
-		System.out.println(xpath);
-		
+
+		//		String currentdate=date.toString();
+		//		
+		//		//Thu Apr 28 18:09:03 IST 2022
+		//		// 0   1   2    3      4    5 
+		//		// Thu Apr 28 2022
+		//		
+		//		String[] arr = currentdate.split(" ");
+		//		
+		//		String day= arr[0];
+		//		String month= arr[1];
+		//		String dt= arr[2];
+		//		String year = arr[5];
+		//		
+		//		String xpath= day+" "+month+" "+dt+" "+year;
+		//		
+		//		System.out.println(xpath);
+
 		WebDriverManager.chromedriver().setup();
 
 		WebDriver driver = new ChromeDriver();
@@ -40,21 +39,31 @@ public class BrowserLAunch {
 		driver.get("https://www.makemytrip.com/");
 
 		driver.manage().window().maximize();
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		Thread.sleep(4000);
-		
+
 		driver.findElement(By.xpath("//span[@class='langCardClose']")).click();
-		
+
 		driver.findElement(By.xpath("//li[@data-cy=\"account\"]")).click();
-		
-		
+
+
 		driver.findElement(By.xpath("//span[.='DEPARTURE']")).click();
 
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//div[@aria-label='"+xpath+"']")).click();
-		
+
+		WebElement nextmonth = driver.findElement(By.xpath("//span[@aria-label='Next Month']"));
+
+		for (int i = 0; i < 12; i++)
+		{
+			try {
+				driver.findElement(By.xpath("//div[@aria-label=\"Sun Sep 04 2022\"]")).click();
+				break;
+			} catch (Exception e) {
+				nextmonth.click();
+			}
+		}
 	}
 
 }
